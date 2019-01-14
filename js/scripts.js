4b0098851451d34259af5c401cfcca70b7c72877
Ian Cook Westgate and Slater Smith <ian@>
2019-01-14 14:40:23 -0800

    create divide, multiply, subtract, and add functions for calculator javascript business end program

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 function add(number1, number2){ return number1 + number2; }
+function subtract(number1, number2){ return number1 - number2; }
+function multiply(number1, number2){ return number1 * number2; }
+function divide(number1, number2){ return number1 / number2; }
 
 var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"))
+var number2 = parseInt(prompt("Enter another number:"));
 
 
-alert(add(number1, number2));
+alert(subtract(number1, number2));
